6aa627af7568b4407649a4184d643884096d6ce9
Oscar Neto <oscar.neto@icarotech.com>
2024-11-01 15:13:10 -0300

    feat: versioning API as v1

---
+++ src/main/java/com/oscarneto/restapi/common/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.oscarneto.restapi.common.utils;
+
+public class Constants {
+
+    private Constants () {}
+
+    public static final String API_VERSION_V1 = "v1";
+}
--- src/main/java/com/oscarneto/restapi/presentation/controllers/VacationController.java
+++ src/main/java/com/oscarneto/restapi/presentation/controllers/VacationController.java
@@ -1,6 +1,7 @@
 package com.oscarneto.restapi.presentation.controllers;
 
 import com.oscarneto.restapi.application.services.VacationService;
+import com.oscarneto.restapi.common.utils.Constants;
 import com.oscarneto.restapi.domain.entities.Vacation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/vacations")
+@RequestMapping(Constants.API_VERSION_V1 + "/vacations")
 public class VacationController {
 
     private final VacationService service;
