f9d29079dcb4f4bd6ba2d99a5240b3887afecc22
Oscar Neto <oscar.neto@icarotech.com>
2024-11-04 17:26:47 -0300

    feat: includes dtos for post operation

--- src/main/java/com/oscarneto/restapi/presentation/dtos/OfferCreateRequestDTO.java
+++ src/main/java/com/oscarneto/restapi/presentation/dtos/OfferCreateRequestDTO.java
@@ -1,9 +1,15 @@
 package com.oscarneto.restapi.presentation.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class OfferCreateRequestDTO {
 
     private Double discount;
